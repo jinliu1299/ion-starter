fbf06151431a58436241134ba57ff498df0b0f8e
jliu <jin.liu@grantsolutions.gov>
2019-11-01 14:35:34 -0400

    Added teset Endpoints

---
+++ src/datomic/ion/sampleEndPoint.clj
@@ -0,0 +1,24 @@
+(ns datomic.ion.sampleEndPoint :require
+  [clojure.data.json :as json]
+  [clojure.edn :as edn]
+  [clojure.java.io :as io]
+  [clojure.pprint :as pp]
+  [datomic.client.api :as d]
+  [datomic.ion.lambda.api-gateway :as apigw])
+
+(defn get-by-type
+  "HTTP handler that returns self-describing info about items matching type."
+  [{:keys [headers body]}]
+  (let [type (some-> body read-edn)]
+    (if (keyword? type)
+      {:status 200
+       :headers {"Content-Type" "application/edn"}
+       :body "200"}
+      {:status 400
+       :headers {}
+       :body "Expected a request body keyword naming a type"})))
+
+
+(def get-by-type-ionized
+  "Ionization of items-by-type for use with AWS API Gateway lambda proxy integration."
+  (apigw/ionize get-by-type))
